534e36e1134b9300aa762df4498ce2c78f1ddf61
mbrovarska <marybrovarska@gmail.com>
2025-04-14 10:39:47 -0400

    PT-28 addresses, specialists inserted, table specialists_services created

--- migrations/000035_create_legal_table.up.sql
+++ migrations/000035_create_legal_table.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE legal (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, 
     document_type  VARCHAR(100),
     document_url TEXT,
-    title VARCHAR(200),
+    title VARCHAR(200)
 );
