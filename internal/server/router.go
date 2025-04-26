0656e5783a082becf2ffc52b80ca9d60b6f67ad1
mbrovarska <marybrovarska@gmail.com>
2025-04-25 21:53:27 -0400

    add code to newginsrver to open 443 port

--- internal/server/router.go
+++ internal/server/router.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
@@ -11,21 +13,44 @@ import (
 func NewGinServer(lc fx.Lifecycle, logger *zap.Logger, server *Server) *gin.Engine {
 	router := gin.Default()
 
+	// lc.Append(fx.Hook{
+	// 	OnStart: func(ctx context.Context) error {
+	// 		go func() {
+	// 			logger.Info("Listening on", zap.String("address", server.config.Address))
+	// 			if err := server.ListenAndServe(router); err != nil {
+	// 				logger.Fatal("Failed to start server", zap.Error(err))
+	// 			}
+	// 		}()
+	// 		return nil
+	// 	},
+	// 	OnStop: func(ctx context.Context) error {
+	// 		logger.Info("Stopping server...")
+	// 		return server.httpServer.Shutdown(ctx)
+	// 	},
+	// })
+
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
-			go func() {
-				logger.Info("Listening on", zap.String("address", server.config.Address))
-				if err := server.ListenAndServe(router); err != nil {
-					logger.Fatal("Failed to start server", zap.Error(err))
-				}
-			}()
-			return nil
+			logger.Info("Listening on", zap.String("address", server.config.Address))
+	
+			env := os.Getenv("ENV")
+			if env == "production" {
+				return server.ListenAndServe(router)
+			} else {
+				go func() {
+					if err := server.ListenAndServe(router); err != nil && err != http.ErrServerClosed {
+						logger.Fatal("Failed to start server", zap.Error(err))
+					}
+				}()
+				return nil
+			}
 		},
 		OnStop: func(ctx context.Context) error {
 			logger.Info("Stopping server...")
 			return server.httpServer.Shutdown(ctx)
 		},
 	})
-
+	
+	
 	return router
 }
