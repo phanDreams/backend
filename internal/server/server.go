c0420d4c987ad49c086209ac12141bbfc42b49fc
mbrovarska <marybrovarska@gmail.com>
2025-04-26 20:52:43 -0400

    ListenAndServe changed to load 443 on aws

--- internal/server/server.go
+++ internal/server/server.go
@@ -40,26 +40,26 @@ func (s *Server) ListenAndServe(router *gin.Engine) error {
 	}
 
 	fmt.Printf("DEBUG Start - TLS_ENABLED='%s', TLS_CERT_FILE='%s', TLS_KEY_FILE='%s'\n",
-        os.Getenv("TLS_ENABLED"), os.Getenv("TLS_CERT_FILE"), os.Getenv("TLS_KEY_FILE"))
-    fmt.Printf("DEBUG TLS startup - certFile='%s' keyFile='%s' enabled=%v\n", s.certFile, s.keyFile, s.tlsConfig.Enabled)
-    fmt.Printf("DEBUG: TLS_ENABLED=%v, CERT_FILE=%s, KEY_FILE=%s\n", s.tlsConfig.Enabled, s.tlsConfig.CertFile, s.tlsConfig.KeyFile)
+		os.Getenv("TLS_ENABLED"), os.Getenv("TLS_CERT_FILE"), os.Getenv("TLS_KEY_FILE"))
+	fmt.Printf("DEBUG TLS startup - certFile='%s' keyFile='%s' enabled=%v\n",
+		s.certFile, s.keyFile, s.tlsConfig.Enabled)
+	fmt.Printf("DEBUG: TLS_ENABLED=%v, CERT_FILE=%s, KEY_FILE=%s\n",
+		s.tlsConfig.Enabled, s.tlsConfig.CertFile, s.tlsConfig.KeyFile)
 
 	if s.certFile != "" && s.keyFile != "" && s.tlsConfig.Enabled {
-        s.logger.Info("Starting HTTPS server...")
-        err := s.httpServer.ListenAndServeTLS(s.certFile, s.keyFile)
-    if err != nil {
-        log.Fatalf("HTTPS server failed: %v", err)
-    	}
-    }
+		s.logger.Info("Starting HTTPS server...")
+		err := s.httpServer.ListenAndServeTLS(s.certFile, s.keyFile)
+		if err != nil {
+			log.Fatalf("HTTPS server failed: %v", err)
+		}
+		return err 
+	}
 
-	fmt .Println("DEBUG certFile:", s.certFile)
+	fmt.Println("DEBUG certFile:", s.certFile)
 	fmt.Println("DEBUG keyFile:", s.keyFile)
 	fmt.Println("DEBUG TLS Enabled:", s.tlsConfig.Enabled)
 
 	s.logger.Info("Starting HTTP server...")
 	err := s.httpServer.ListenAndServe()
-	if err != nil {
-		log.Fatalf("HTTP server failed: %v", err)
-	}
-	return nil
+	return err
 }
